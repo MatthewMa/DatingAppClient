7bd7ac7cd15cb6f8aa55956961355727748567fd
MatthewMa <masihua721@gmail.com>
2022-01-12 23:36:25 -0700

    initial app dating client project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Constants } from './constants/constants';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'DatingAppClient';
+export class AppComponent implements OnInit {
+  constructor(private http: HttpClient) {}
+  ngOnInit(): void {
+    this.http.get(Constants.BASE_URL + 'users').subscribe((data) => {
+      console.log(data);
+    });
+  }
+
 }
---
+++ src/app/constants/constants.ts
@@ -0,0 +1,3 @@
+export class Constants {
+  public static readonly BASE_URL = 'https://localhost:5001/api/';
+}
